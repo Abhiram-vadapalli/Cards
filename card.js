c5b313d59151575fd5c36ae9fca20e33afc80e0b
Abhi_vadapalli0604 <111443782+Abhiram-vadapalli@users.noreply.github.com>
2024-07-18 22:49:00 +0530

    Add files via upload

---
+++ card.js
@@ -0,0 +1,7 @@
+function redirectToValorant() {
+    window.location.href = 'https://playvalorant.com/';
+}
+
+function redirectToCallOfDuty() {
+    window.location.href = 'https://www.callofduty.com/';
+}
